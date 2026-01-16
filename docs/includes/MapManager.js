98f661eed8657a066531b8062ac3d7eb8204c0df
Roman Schlagowsky <roman@schlagowsky.net>
2026-01-16 21:00:25 +0100

    feat: Extract MapManager into separate file

---
+++ docs/includes/MapManager.js
@@ -0,0 +1,231 @@
+/**
+ * MapManager handles the creation and management of an interactive map displaying
+ * location data from GeoJSON files. It integrates with Leaflet for map rendering,
+ * supports clustering, custom markers, and category filtering.
+ */
+class MapManager {
+
+    /**
+     * Creates a new MapManager instance and initializes the interactive map.
+     * 
+     * @param {string} mapElementId - The ID of the HTML element where the map will be rendered
+     * @param {Object} options - Configuration options for the map
+     * @param {boolean} [options.showLocateButton=false] - Show user location button
+     * @param {boolean} [options.showCategorySelection=true] - Show category dropdown
+     * @param {number} [options.clusterBelowZoom] - Zoom level threshold for marker clustering
+     * @param {Function} [options.onDataReady] - Callback function called after data loads with categories array
+     * @param {boolean} [options.useCustomMarkers=false] - Use custom marker images from properties
+     * @param {boolean} [options.isFullScreen=false] - Enable full-screen map mode
+     * @param {string} [options.mapBoxKey] - API key for Mapbox (enables Mapbox tiles)
+     * @param {string} [options.mapBoxStyle] - Mapbox style ID for custom map styling
+     * @param {string} [dataFolder='../data/'] - Path to data folder containing images and GeoJSON files
+     * 
+     * @throws {Error} If the map element does not exist in the DOM
+     */
+    constructor(mapElementId, options = {}, dataFolder = '../data/') {
+        // Apply options.
+        this.clusterZoom = options.clusterBelowZoom
+        this.showLocateButton = (options.showLocateButton === true)
+        this.showCategorySelection = options.showCategorySelection
+        this.onDataReady = options.onDataReady
+        this.useCustomMarkers = (options.useCustomMarkers === true)
+
+        // Initialize members.
+        this.mainCategories = new Array();
+        this.categories = new Object();
+        this.currentCategory = 'all'
+        this.map = undefined
+        this.isLocal = location.hostname == 'localhost' || location.hostname == '192.168.2.169'
+        this.repositoryBaseUrl = 'https://cdn.jsdelivr.net/gh/rapscript/cypher-map@master/'
+        this.markerLayer = undefined
+        this.useClustering = (this.clusterZoom !== undefined && typeof (this.clusterZoom) == 'number')
+        // GeoJSON data parsed from .geojson file.
+        this.geoJson = undefined
+        this.dataFolder = dataFolder
+
+        // Add loading layer DOM.
+        let mapContainer = document.getElementById(mapElementId)
+        mapContainer.classList.add('lh-mp-ctnr')
+        mapContainer.innerHTML = '<div id="loading"><svg height="100" width="100" class="spinner"><circle cx="50" cy="50" r="20" class="inner-circle" /></svg></div>'
+
+        this.map = this.createMap(mapElementId, options);
+    }
+
+    createMap(mapElementId, { mapBoxKey, mapBoxStyle, isFullScreen }) {
+
+        const windowHeight = window.innerHeight
+        const mapHeight = document.getElementById(mapElementId).clientHeight
+
+        const map = L.map(mapElementId, {
+            zoomControl: false,
+            scrollWheelZoom: isFullScreen || (mapHeight / windowHeight) < 0.85,
+            dragging: isFullScreen || !L.Browser.mobile,
+            tap: isFullScreen || !L.Browser.mobile,
+        }).setView([51.3396955, 12.3730747], 13);
+
+        L.control.zoom({ position: 'bottomleft' }).addTo(map)
+
+        if (mapBoxKey !== undefined && typeof (mapBoxKey) == 'string' && mapBoxKey.length > 0) {
+            // Use Mapbox if key is provided.
+            const mapboxAttribution = 'Data by <a href="http://rapscript.de/" target="_blank">rapscript.de</a> | ' +
+                '<a href="https://github.com/RapScript/cypher-map" target="_blank">Code</a> on GitHub' +
+                '<br>Map data &copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, ' +
+                '<a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>, ' +
+                'Imagery © <a href="https://www.mapbox.com/" target="_blank">Mapbox</a>'
+            const retinaPart = (window.devicePixelRatio > 1) ? '@2x' : ''
+            const useCustomStyle = (mapBoxStyle !== undefined && typeof (mapBoxStyle) == 'string' && mapBoxStyle.length > 0)
+
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}' + retinaPart + '?access_token={accessToken}', {
+                attribution: mapboxAttribution,
+                maxZoom: 18,
+                id: (useCustomStyle ? mapBoxStyle : 'mapbox/streets-v11'),
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: mapBoxKey,
+            }).addTo(map);
+        } else {
+            // Use OpenStreetMap as fallback.
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
+                foo: 'bar',
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+            }).addTo(map);
+        }
+
+        if (this.showLocateButton) {
+            CypherMapDOMHelper.loadScript('https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.71.1/dist/L.Control.Locate.min.js', () => {
+                L.control.locate({ position: 'bottomleft', showCompass: false }).addTo(map);
+            })
+        }
+
+        return map
+    }
+
+    onEachMapFeature(feature, layer) {
+        const coord = feature.geometry.coordinates
+        const address = '<a onclick="RapScriptHelper.navigate(' + coord[1] + ',' + coord[0] + ')" class="directions-link" title="Route anzeigen">' +
+            '<i class="fa fa-directions"></i></a>'/* +
+            feature.properties.address.split(', ').join('<br>') */
+        // does this feature have a property named popupContent?
+        if (feature.properties && feature.properties.name && feature.properties.description) {
+            layer.bindPopup(
+                '<h3>' + feature.properties.name + '</h3><p>' + feature.properties.description + '</p><p>' + address + '</p>');
+        }
+    }
+
+    renderMapMarker(geoJsonPoint, coordinatate) {
+        const markerFilename = (this.useCustomMarkers ? geoJsonPoint.properties.image : 'marker_default.svg')
+        const icon = L.icon({
+            iconUrl: this.dataFolder + 'images/' + markerFilename,
+            iconSize: [38, 38],
+            shadowUrl: this.dataFolder + 'images/shadow.svg',
+            shadowSize: (this.useCustomMarkers ? [50, 50] : [7, 7]),
+            shadowAnchor: (this.useCustomMarkers ? [25, 22] : [3.5, -15])
+        });
+        return L.marker(coordinatate, { icon: icon })
+            .bindTooltip(geoJsonPoint.properties.name, { offset: [0, 16] })
+    }
+
+    setMarkerLayer(layer, map, zoomToSelection = false) {
+
+        if (this.markerLayer !== undefined) {
+            map.removeLayer(this.markerLayer)
+        }
+
+        if (this.useClustering) {
+
+            const addClusterLayer = (layer, map) => {
+                const markers = L.markerClusterGroup({
+                    disableClusteringAtZoom: this.clusterZoom
+                });
+                markers.addLayer(layer)
+                this.markerLayer = markers
+                map.addLayer(markers)
+            }
+
+            if (L.markerClusterGroup === undefined) {
+                CypherMapDOMHelper.loadScript('https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', () => {
+                    addClusterLayer(layer, map)
+                })
+            } else {
+                addClusterLayer(layer, map)
+            }
+        } else {
+            layer.addTo(map)
+            this.markerLayer = layer
+        }
+
+        if (zoomToSelection) {
+            map.fitBounds(L.featureGroup([this.markerLayer]).getBounds())
+        }
+    }
+
+    applyFilter(filterCategory, zoomToSelection = false) {
+
+        const geoLayer = L.geoJSON(this.geoJson, {
+            onEachFeature: this.onEachMapFeature,
+            pointToLayer: (point, coord) => this.renderMapMarker(point, coord),
+            filter: function (feature, layer) {
+                const featureCategory = feature.properties.category
+                return (featureCategory == filterCategory || filterCategory == 'all')
+            }
+        })
+
+        this.setMarkerLayer(geoLayer, this.map, zoomToSelection)
+    }
+
+    selectCategory(selectedCategory) {
+        this.applyFilter(selectedCategory, true)
+        this.currentCategory = selectedCategory
+    }
+
+    applyGeoData(data) {
+
+        this.geoJson = JSON.parse(data);
+        const features = this.geoJson['features'];
+
+        // Collect categories.
+        for (const feature in features) {
+            const properties = features[feature]['properties']
+            const category = properties['category']
+
+            if (category !== undefined && !this.categories.hasOwnProperty(category)) {
+                this.categories[category] = true
+            }
+        }
+        this.mainCategories = Object.keys(this.categories).sort()
+        console.log(this.categories);
+
+        if (this.showCategorySelection !== false && this.mainCategories.length > 0) {
+            // Create category selection control.
+            const control = L.control({ position: 'topright' });
+            control.onAdd = (map) => {
+                const div = L.DomUtil.create('div', 'command');
+
+                let categorySelection = '<form><div class="select-wrapper fa fa-angle-down"><select id="category-selection" name="category">'
+                categorySelection += '<option value="all">Alle</option>'
+                for (const catId in this.mainCategories) {
+                    let category = this.mainCategories[catId]
+                    categorySelection += '<option value="' + category + '">' + category + '</option>'
+                }
+                categorySelection += '</select></div></form>'
+
+                div.innerHTML = categorySelection;
+                return div;
+            };
+            control.addTo(this.map);
+            document
+                .getElementById('category-selection')
+                .addEventListener('change', (event) => this.selectCategory(event.target.value), false);
+        }
+
+        this.applyFilter('all', true);
+
+        // Remove loading overlay.
+        document.getElementById('loading').remove()
+
+        // Call handler.
+        if (this.onDataReady !== undefined && typeof (this.onDataReady) == 'function') {
+            this.onDataReady(this.mainCategories)
+        }
+    }
+}
--- docs/map.js
+++ docs/map.js
@@ -20,231 +20,38 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-class RapScriptMap {
-
+class FreestyleRapCypherMap {
     constructor(mapElementId, options = {}) {
-        this.mainCategories = new Array();
-        this.categories = new Object();
-        this.currentCategory = 'all'
-        this.map = undefined
         this.isLocal = location.hostname == 'localhost' || location.hostname == '192.168.2.169'
+        this.mapManager = undefined
         this.repositoryBaseUrl = 'https://cdn.jsdelivr.net/gh/rapscript/cypher-map@master/'
-        this.clusterZoom = options.clusterBelowZoom
-        this.markerLayer = undefined
-        this.useClustering = (this.clusterZoom !== undefined && typeof (this.clusterZoom) == 'number')
-        this.showLocateButton = (options.showLocateButton === true)
-        this.showCategorySelection = options.showCategorySelection
-        this.onDataReady = options.onDataReady
-        this.useCustomMarkers = (options.useCustomMarkers === true)
-        // GeoJSON data parsed from .geojson file.
-        this.geoJson = undefined
 
-        // Add loading layer DOM.
-        let mapContainer = document.getElementById(mapElementId)
-        mapContainer.classList.add('lh-mp-ctnr')
-        mapContainer.innerHTML = '<div id="loading"><svg height="100" width="100" class="spinner"><circle cx="50" cy="50" r="20" class="inner-circle" /></svg></div>'
-
-        const resourceVersionTag = '20200619'
-        const dataUrl = (this.isLocal ? '../' : this.repositoryBaseUrl) + 'data/italy.geojson?v=' + resourceVersionTag
+        const resourceVersionTag = '202006120'
+        const dataFolder = (this.isLocal ? '../' : this.repositoryBaseUrl) + 'data/'
+        const dataUrl = dataFolder + 'italy.geojson?v=' + resourceVersionTag
         const cssUrl = (this.isLocal ? '' : this.repositoryBaseUrl + 'docs/') + 'map-style.css?v=' + resourceVersionTag
 
-        RapScriptHelper.loadCss(cssUrl)
-        RapScriptHelper.loadCss('https://use.fontawesome.com/releases/v5.8.1/css/all.css')
-        RapScriptHelper.loadCss('https://unpkg.com/leaflet@1.6.0/dist/leaflet.css')
-        RapScriptHelper.loadScript('https://unpkg.com/leaflet@1.6.0/dist/leaflet.js', () => {
-            this.map = this.createMap(mapElementId, options);
-            RapScriptHelper.loadUrl(dataUrl, (data) => this.applyGeoData(data));
+        CypherMapDOMHelper.loadCss(cssUrl)
+        CypherMapDOMHelper.loadCss('https://use.fontawesome.com/releases/v5.8.1/css/all.css')
+        CypherMapDOMHelper.loadCss('https://unpkg.com/leaflet@1.6.0/dist/leaflet.css')
+        CypherMapDOMHelper.loadScript('https://unpkg.com/leaflet@1.6.0/dist/leaflet.js', () => {
+            CypherMapDOMHelper.loadScript('includes/MapManager.js', () => {
+                this.mapManager = new MapManager( mapElementId, options, dataFolder )
+                CypherMapDOMHelper.loadUrl( dataUrl, (data) => this.mapManager.applyGeoData(data) );
+            })
         })
         // Add cluster css when clustering is enabled.
         if (this.clusterZoom !== undefined && typeof (this.clusterZoom) == 'number') {
-            RapScriptHelper.loadCss('https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css')
-            RapScriptHelper.loadCss('https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css')
+            CypherMapDOMHelper.loadCss('https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css')
+            CypherMapDOMHelper.loadCss('https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css')
         }
         if (this.showLocateButton) {
-            RapScriptHelper.loadCss('https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.71.1/dist/L.Control.Locate.min.css')
-        }
-    }
-
-    createMap(mapElementId, { mapBoxKey, mapBoxStyle, isFullScreen }) {
-
-        const windowHeight = window.innerHeight
-        const mapHeight = document.getElementById(mapElementId).clientHeight
-
-        const map = L.map(mapElementId, {
-            zoomControl: false,
-            scrollWheelZoom: isFullScreen || (mapHeight / windowHeight) < 0.85,
-            dragging: isFullScreen || !L.Browser.mobile,
-            tap: isFullScreen || !L.Browser.mobile,
-        }).setView([51.3396955, 12.3730747], 13);
-
-        L.control.zoom({ position: 'bottomleft' }).addTo(map)
-
-        if (mapBoxKey !== undefined && typeof (mapBoxKey) == 'string' && mapBoxKey.length > 0) {
-            // Use Mapbox if key is provided.
-            const mapboxAttribution = 'Data by <a href="http://rapscript.de/" target="_blank">rapscript.de</a> | ' +
-                '<a href="https://github.com/RapScript/cypher-map" target="_blank">Code</a> on GitHub' +
-                '<br>Map data &copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/" target="_blank">Mapbox</a>'
-            const retinaPart = (window.devicePixelRatio > 1) ? '@2x' : ''
-            const useCustomStyle = (mapBoxStyle !== undefined && typeof (mapBoxStyle) == 'string' && mapBoxStyle.length > 0)
-
-            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}' + retinaPart + '?access_token={accessToken}', {
-                attribution: mapboxAttribution,
-                maxZoom: 18,
-                id: (useCustomStyle ? mapBoxStyle : 'mapbox/streets-v11'),
-                tileSize: 512,
-                zoomOffset: -1,
-                accessToken: mapBoxKey,
-            }).addTo(map);
-        } else {
-            // Use OpenStreetMap as fallback.
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
-                foo: 'bar',
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-            }).addTo(map);
-        }
-
-        if (this.showLocateButton) {
-            RapScriptHelper.loadScript('https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.71.1/dist/L.Control.Locate.min.js', () => {
-                L.control.locate({ position: 'bottomleft', showCompass: false }).addTo(map);
-            })
-        }
-
-        return map
-    }
-
-    onEachMapFeature(feature, layer) {
-        const coord = feature.geometry.coordinates
-        const address = '<a onclick="RapScriptHelper.navigate(' + coord[1] + ',' + coord[0] + ')" class="directions-link" title="Route anzeigen">' +
-            '<i class="fa fa-directions"></i></a>'/* +
-            feature.properties.address.split(', ').join('<br>') */
-        // does this feature have a property named popupContent?
-        if (feature.properties && feature.properties.name && feature.properties.description) {
-            layer.bindPopup(
-                '<h3>' + feature.properties.name + '</h3><p>' + feature.properties.description + '</p><p>' + address + '</p>');
-        }
-    }
-
-    renderMapMarker(geoJsonPoint, coordinatate) {
-        const markerFilename = (this.useCustomMarkers ? geoJsonPoint.properties.image : 'marker_default.svg')
-        const icon = L.icon({
-            iconUrl: (this.isLocal ? '../' : this.repositoryBaseUrl) + 'data/images/' + markerFilename,
-            iconSize: [38, 38],
-            shadowUrl: (this.isLocal ? '' : this.repositoryBaseUrl + 'docs/') + 'shadow.svg',
-            shadowSize: (this.useCustomMarkers ? [50, 50] : [7, 7]),
-            shadowAnchor: (this.useCustomMarkers ? [25, 22] : [3.5, -15])
-        });
-        return L.marker(coordinatate, { icon: icon })
-            .bindTooltip(geoJsonPoint.properties.name, { offset: [0, 16] })
-    }
-
-    setMarkerLayer(layer, map, zoomToSelection = false) {
-
-        if (this.markerLayer !== undefined) {
-            map.removeLayer(this.markerLayer)
-        }
-
-        if (this.useClustering) {
-
-            const addClusterLayer = (layer, map) => {
-                const markers = L.markerClusterGroup({
-                    disableClusteringAtZoom: this.clusterZoom
-                });
-                markers.addLayer(layer)
-                this.markerLayer = markers
-                map.addLayer(markers)
-            }
-
-            if (L.markerClusterGroup === undefined) {
-                RapScriptHelper.loadScript('https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js', () => {
-                    addClusterLayer(layer, map)
-                })
-            } else {
-                addClusterLayer(layer, map)
-            }
-        } else {
-            layer.addTo(map)
-            this.markerLayer = layer
-        }
-
-        if (zoomToSelection) {
-            map.fitBounds(L.featureGroup([this.markerLayer]).getBounds())
-        }
-    }
-
-    applyFilter(filterCategory, zoomToSelection = false) {
-
-        const geoLayer = L.geoJSON(this.geoJson, {
-            onEachFeature: this.onEachMapFeature,
-            pointToLayer: (point, coord) => this.renderMapMarker(point, coord),
-            filter: function (feature, layer) {
-                const featureCategory = feature.properties.category
-                return (featureCategory == filterCategory || filterCategory == 'all')
-            }
-        })
-
-        this.setMarkerLayer(geoLayer, this.map, zoomToSelection)
-    }
-
-    selectCategory(selectedCategory) {
-        this.applyFilter(selectedCategory, true)
-        this.currentCategory = selectedCategory
-    }
-
-    applyGeoData(data) {
-
-        this.geoJson = JSON.parse(data);
-        const features = this.geoJson['features'];
-
-        // Collect categories.
-        for (const feature in features) {
-            const properties = features[feature]['properties']
-            const category = properties['category']
-
-            if (category !== undefined && !this.categories.hasOwnProperty(category)) {
-                this.categories[category] = true
-            }
-        }
-        this.mainCategories = Object.keys(this.categories).sort()
-        console.log(this.categories);
-
-        if (this.showCategorySelection !== false && this.mainCategories.length > 0) {
-            // Create category selection control.
-            const control = L.control({ position: 'topright' });
-            control.onAdd = (map) => {
-                const div = L.DomUtil.create('div', 'command');
-
-                let categorySelection = '<form><div class="select-wrapper fa fa-angle-down"><select id="category-selection" name="category">'
-                categorySelection += '<option value="all">Alle</option>'
-                for (const catId in this.mainCategories) {
-                    let category = this.mainCategories[catId]
-                    categorySelection += '<option value="' + category + '">' + category + '</option>'
-                }
-                categorySelection += '</select></div></form>'
-
-                div.innerHTML = categorySelection;
-                return div;
-            };
-            control.addTo(this.map);
-            document
-                .getElementById('category-selection')
-                .addEventListener('change', (event) => this.selectCategory(event.target.value), false);
-        }
-
-        this.applyFilter('all', true);
-
-        // Remove loading overlay.
-        document.getElementById('loading').remove()
-
-        // Call handler.
-        if (this.onDataReady !== undefined && typeof (this.onDataReady) == 'function') {
-            this.onDataReady(this.mainCategories)
+            CypherMapDOMHelper.loadCss('https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.71.1/dist/L.Control.Locate.min.css')
         }
     }
 }
 
-class RapScriptHelper {
+class CypherMapDOMHelper {
 
     static loadScript(url, callback = () => { }) {
         const scriptNode = document.createElement("script");
